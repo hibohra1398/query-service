0c418e01eb0bc575d8a9ba0dd64d236858601d38
Mihir Gore <mihir.gore@traceable.ai>
2024-11-12 15:52:02 +0530

    Upgrade to guice 6

--- query-service-factory/build.gradle.kts
+++ query-service-factory/build.gradle.kts
@@ -6,5 +6,5 @@ dependencies {
   api("org.hypertrace.core.serviceframework:platform-grpc-service-framework:0.1.78")
 
   implementation(project(":query-service-impl"))
-  implementation("com.google.inject:guice:5.0.1")
+  implementation("com.google.inject:guice:6.0.0")
 }
